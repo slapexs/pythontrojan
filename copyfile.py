208b55a72baf5ef8d17332cfa47a50d8abdc7024
onze <slapexs@gmail.com>
2022-07-03 13:40:47 +0700

    Change variable couter and change program to while loop

--- copyfile.py
+++ copyfile.py
@@ -7,9 +7,11 @@ path = os.path.join(cwd, 'test')
 targetFile = os.path.join(path, 'dontopen.txt')
 
 fileName = 'dontopen1.txt'
+count = 1
 while True:
     copyFile = os.path.join(path, fileName)
     shutil.copyfile(targetFile, copyFile)
-    fileName = 'dontopen{}.txt'.format(i)
-    print(fileName)
+    fileName = 'dontopen{}.txt'.format(count)
+    count += 1
     time.sleep(2)
+    
